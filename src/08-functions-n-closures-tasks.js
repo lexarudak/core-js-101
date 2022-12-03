62454cbe2a438daac27d9e9ffa353f086bf1b396
Aliaksei Rudak <lexarudak@gmail.com>
2022-12-03 18:07:44 +0600

    feat: solve task

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -44,8 +44,11 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  const a = exponent;
+  return function (val) {
+    return val ** a;
+  };
 }
 
 
